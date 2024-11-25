9c5af67ad86f94717a4615655268fb13d1a765b6
Ричард Громов <GromovRP23@vvsu.ithub.ru>
2024-11-25 18:13:30 +1000

    Добавьте файлы проекта.

---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            byte red = (byte)random.Next(255);
+            byte green = (byte)random.Next(255);
+            byte blue = (byte)random.Next(255);
+
+            Color randomcolor = Color.FromRgb(red, green, blue);
+            Background = new SolidColorBrush(randomcolor);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Image ImageContainer = new Image();
+            ImageSource image = new BitmapImage(new Uri(Environment.CurrentDirectory + "/image/print/gerb.jpg", UriKind.Absolute));
+            ImageContainer.Source = image;
+        }
+    }
+}
+
